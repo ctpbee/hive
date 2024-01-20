90bb17407cfaca1699392d2817b0935a62a52568
somewheve <somewheve@gmail.com>
2024-01-20 22:40:23 +0800

    delete build

--- build/lib/hive/log.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-logger = logging.Logger(name="hive")
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-ch = logging.StreamHandler()
-ch.setLevel(logging.INFO)
-ch.setFormatter(formatter)
-logger.addHandler(ch)
